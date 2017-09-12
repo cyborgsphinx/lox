e409bbbbaa8d53b3469824a65e1c94581d4e676f
James Hannah <jhannah8124@gmail.com>
2017-09-11 23:59:04 -0700

    implemented statements and environment

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Implementations of the language described in [Crafting Interpreters](http://www.craftinginterpreters.com/)
 
 This is split between two projects, jlox and clox. jlox is the version done in Java, and clox is done in C
+
+I will be using checked exceptions as much as I can.
+This seems to be limited to the parser unless I want to change the entire interface for the visitors to allow the interpreter to have them as well.
+Since the interpreter is creating runtime exceptions anyway, it does make some sense for them to be runtime Java exceptions.
+
+I just want more exceptions to be checked, so I'm doing that when I can.
--- jlox/src/com/cyborgsphinx/lox/Parser.java
+++ jlox/src/com/cyborgsphinx/lox/Parser.java
@@ -1,7 +1,6 @@
 package com.cyborgsphinx.lox;
 
-import com.sun.tools.javac.parser.Tokens;
-
+import java.util.ArrayList;
 import java.util.List;
 
 class Parser {
@@ -13,17 +12,85 @@ class Parser {
         this.tokens = tokens;
     }
 
-    Expr parse() {
+    List<Stmt> parse() {
+        List<Stmt> statements = new ArrayList<>();
+        while (!isAtEnd()) {
+            statements.add(declaration());
+        }
+        return statements;
+    }
+
+    private Stmt declaration() {
         try {
-            return expression();
+            if (match(TokenType.VAR)) {
+                return varDeclaration();
+            } else {
+                return statement();
+            }
         } catch (ParseError error) {
-            // panic mode recovery
+            // panic mode handling
+            synchronize();
             return null;
         }
     }
 
+    private Stmt statement() throws ParseError {
+        if (match(TokenType.PRINT)) {
+            return printStatement();
+        } else if (match(TokenType.LEFT_BRACE)) {
+            return new Stmt.Block(block());
+        } else {
+            return expressionStatement();
+        }
+    }
+
+    private Stmt printStatement() throws ParseError {
+        Expr value = expression();
+        consume(TokenType.SEMICOLON, "Expect ';' after value.");
+        return new Stmt.Print(value);
+    }
+
+    private Stmt varDeclaration() throws ParseError {
+        Token name = consume(TokenType.IDENTIFIER, "Expect variable name.");
+        Expr initializer = null;
+        if (match(TokenType.EQUAL)) {
+            initializer = expression();
+        }
+        consume(TokenType.SEMICOLON, "Expect ';' after variable declaration.");
+        return new Stmt.Var(name, initializer);
+    }
+
+    private Stmt expressionStatement() throws ParseError {
+        Expr expr = expression();
+        consume(TokenType.SEMICOLON, "Expect ';' after value.");
+        return new Stmt.Expression(expr);
+    }
+
+    private List<Stmt> block() throws ParseError {
+        List<Stmt> statements = new ArrayList<>();
+        while (!check(TokenType.RIGHT_BRACE) && !isAtEnd()) {
+            statements.add(declaration());
+        }
+        consume(TokenType.RIGHT_BRACE, "Expect '}' after block.");
+        return statements;
+    }
+
     private Expr expression() throws ParseError {
-        return equality();
+        return assignment();
+    }
+
+    private Expr assignment() throws ParseError {
+        Expr expr = equality();
+        if (match(TokenType.EQUAL)) {
+            Token equals = previous();
+            Expr value = assignment();
+            if (expr instanceof Expr.Variable) {
+                Token name = ((Expr.Variable)expr).name;
+                return new Expr.Assign(name, value);
+            }
+            error(equals, "Invalid assignment target.");
+        }
+        return expr;
     }
 
     private Expr equality() throws ParseError {
@@ -78,22 +145,24 @@ class Parser {
 
 
     private Expr primary() throws ParseError {
-        if (match(TokenType.FALSE)) { return new Expr.Literal(false); }
-        if (match(TokenType.TRUE)) { return new Expr.Literal(true); }
-        if (match(TokenType.NIL)) { return new Expr.Literal(null); }
-
-        if (match(TokenType.NUMBER, TokenType.STRING)) {
+        if (match(TokenType.FALSE)) {
+            return new Expr.Literal(false);
+        } else if (match(TokenType.TRUE)) {
+            return new Expr.Literal(true);
+        } else if (match(TokenType.NIL)) {
+            return new Expr.Literal(null);
+        } else if (match(TokenType.NUMBER, TokenType.STRING)) {
             return new Expr.Literal(previous().literal);
-        }
-
-        if (match(TokenType.LEFT_PAREN)) {
+        } else if (match(TokenType.IDENTIFIER)) {
+            return new Expr.Variable(previous());
+        } else if (match(TokenType.LEFT_PAREN)) {
             Expr expr = expression();
             consume(TokenType.RIGHT_PAREN, "Expect ')' after expression.");
             return new Expr.Grouping(expr);
+        } else {
+            // nothing found - throw for now
+            throw error(peek(), "Expected expression.");
         }
-
-        // nothing found - throw for now
-        throw error(peek(), "Expected expression.");
     }
 
     private boolean match(TokenType... types) {
