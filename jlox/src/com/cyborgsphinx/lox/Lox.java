bfe14ee68bdd75ae6b6eea2afa49294e0051bede
James Hannah <jhannah8124@gmail.com>
2017-09-11 22:39:02 -0700

    implemented expression evaluation

--- jlox/src/com/cyborgsphinx/lox/Lox.java
+++ jlox/src/com/cyborgsphinx/lox/Lox.java
@@ -11,6 +11,9 @@ import java.util.List;
 
 public class Lox {
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
+    private static final Interpreter interpreter = new Interpreter();
+
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
@@ -27,6 +30,7 @@ public class Lox {
 
         // exit with non-zero exit code to indicate error
         if (hadError) { System.exit(65); }
+        if (hadRuntimeError) { System.exit(70); }
     }
 
     private static void runPrompt() throws IOException {
@@ -49,7 +53,7 @@ public class Lox {
         // Stop if there was a syntax error
         if (hadError) { return; }
 
-        System.out.println(new AstPrinter().print(expression));
+        interpreter.interpret(expression);
     }
 
     static void error(int line, String message) {
@@ -59,6 +63,11 @@ public class Lox {
     static void error(Token token, String message) {
         String location = token.type == TokenType.EOF ? " at end" : " at '" + token.lexeme + "'";
         report(token.line, location, message);
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 
     private static void report(int line, String where, String message) {
